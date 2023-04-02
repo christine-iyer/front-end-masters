3a3054eaa61ef911c8484daa9b06fb9ed7ff5f76
chrisIyer <hellohotbox420@gmail.com>
2023-04-02 09:59:32 -0400

    new files

--- lessons/09-hooks-in-depth/higherOrderFunctions.js
+++ lessons/09-hooks-in-depth/higherOrderFunctions.js
@@ -3,3 +3,22 @@
 for(let i = 0;i<10;i++) {
      console.log(i)
 }
+function calculate(a, b) {
+     const add = a + b;
+     const subtract = a - b;
+     const multiply = a * b;
+     const divide = (a / b).toFixed(2);
+     const average = (add/2)
+   
+     return [add, subtract, multiply, divide,average];
+   }
+   
+   const [add, subtract, multiply, divide,average] = calculate(10, 19);
+   
+   if (authenticated) {
+     renderApp();
+   } else {
+     renderLogin();
+   }
+   authenticated ? renderApp() : renderLogin();
+   
